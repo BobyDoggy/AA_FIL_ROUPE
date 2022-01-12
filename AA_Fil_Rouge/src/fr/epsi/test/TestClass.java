236de1a40336215d0638d724fe028a123ae2bbe9
OussamaSJ <oussam.jb44@gmail.com>
2022-01-12 17:23:28 +0100

    //

--- AA_Fil_Rouge/src/fr/epsi/test/TestClass.java
+++ AA_Fil_Rouge/src/fr/epsi/test/TestClass.java
@@ -2,10 +2,12 @@ package fr.epsi.test;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.Mock;
  
 class TestClass {
- 
-	@Test	
+    @Mock
+	private String test;
+	
 	public void Test1 () {
 		
 	}
